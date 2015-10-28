b55d8f3a2a866f8fedc4a96929bb72d49a9b97fe
Prasad <silvaprasa@gmail.com>
2015-10-28 23:54:56 +0530

    Completed display salesdata

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -5,8 +5,9 @@ public class SalesData {
 	int data[] = { 0, 4, 42 };
 
 	public void display() {
-		for (int i : data) {
-			System.out.println("");
+		System.out.println("Data: ");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 }
